c9dd28514f8052fa43043b1f427a94a6089dda68
Maripontre <114959642+Maripontre@users.noreply.github.com>
2024-09-23 17:31:17 -0300

    Add files via upload

--- aprendiz1/conecta.php
+++ aprendiz1/conecta.php
@@ -1,9 +1,9 @@
 <?php
-/*
+
     $con = mysqli_init();
     mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
     mysqli_real_connect($conn, "tcc2024mysql.mysql.database.azure.com", "tcc",  "{543150Ifsp}", "{tcc}", 3306, MYSQLI_CLIENT_SSL);	
-*/
+/*
     $DSN = "mysql:host="tcc2024mysql.mysql.database.azure.com";
             dbname="crud_ai";charset=utf8";
 	$USUARIO = "tcc";
